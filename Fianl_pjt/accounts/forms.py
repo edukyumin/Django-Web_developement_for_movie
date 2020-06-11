21368efbfd854b53fed6ac679d4eb6b1a7af849b
이규민 <2kyumin3@gmail.com>
2020-06-11 15:13:04 +0900

    0101

---
+++ Fianl_pjt/accounts/forms.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()

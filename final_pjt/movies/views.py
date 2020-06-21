6543ee72ca1b5932d231301c027ea6a9dd0a73ec
이규민 <2kyumin3@gmail.com>
2020-06-21 20:46:44 +0900

    Final_pjt End

--- final_pjt/movies/views.py
+++ final_pjt/movies/views.py
@@ -17,7 +17,8 @@ def index(request):
     page_obj = paginator.get_page(page_number)
 
     
-    # YOUTUBE_API_KEY='AIzaSyDZ8volmsp-EzcqFDGcTqJ-yIslbuS6lZA'
+    # YOUTUBE_API_KEY='AIzaSyARmKeLqm3doSnkEbYhGjRkxXj-25YuYd8'
+    
     # YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search'
     # random_number = range(0, 9)
     # top_movies = movies[int(random.choice(random_number))]
@@ -43,11 +44,11 @@ def index(request):
     return render(request, 'movies/index.html', context)
 
 ####################################################################################
-# MOVIE #
+## MOVIE ##
 
 @login_required
 def movie_create(request):
-    # superuser만 movie_create할 수 있음
+    ## superuser만 movie_create할 수 있음
     if not request.user.is_superuser:
         return redirect('movies:index')
     if request.method == 'POST':
@@ -93,7 +94,7 @@ def movie_delete(request, movie_pk):
     return redirect('movies:index')
 
 ##################################################################################
-# REVIEW #
+## REVIEW ##
 
 @login_required
 def create(request, movie_pk):
@@ -118,9 +119,9 @@ def detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     reviews = movie.review_set.all()
     genres = movie.genre_ids.all()
-    # youtube에서 트레일러 가져오기
+    ## youtube에서 트레일러 가져오기
 
-    # YOUTUBE_API_KEY='AIzaSyDZ8volmsp-EzcqFDGcTqJ-yIslbuS6lZA'
+    # YOUTUBE_API_KEY='AIzaSyARmKeLqm3doSnkEbYhGjRkxXj-25YuYd8'
     # YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search'
     # params = {
     #     'key': YOUTUBE_API_KEY,
@@ -164,7 +165,6 @@ def update(request, movie_pk, review_pk):
                 review = form.save(commit=False)
                 review.user = request.user
                 review.save()
-                # return redirect('movies:index')
                 return redirect('movies:review_detail', movie_pk, review.pk)
         else:
             form = ReviewForm(instance=review)
@@ -184,7 +184,7 @@ def delete(request,movie_pk, review_pk):
     return redirect('movies:detail', movie_pk)
 
 #######################################################################################
-# COMMENT #
+## COMMENT ##
 
 @login_required
 def comments_create(request, movie_pk, review_pk):
@@ -214,12 +214,12 @@ def like(request, movie_pk):
     user = request.user
     movie = get_object_or_404(Movie, pk=movie_pk)
 
-    # 좋아요 취소
+    ## 좋아요 취소
     if movie.like_users.filter(pk=user.pk).exists():
         movie.like_users.remove(user)
         liked = False
 
-    # 좋아요
+    ## 좋아요
     else:
         movie.like_users.add(user)
         liked = True
@@ -232,7 +232,8 @@ def like(request, movie_pk):
 
 
 #################################################################################
-# Genre #
+## 아래는 장르를 생성하는 관리자용 기능. (사용하지 않았음)
+## Genre ##
 
 # @login_required
 # def genre_create(request):
@@ -279,8 +280,8 @@ def like(request, movie_pk):
 #         genre.delete()
 #     return redirect('movies:index')
 
-###########################################################################################
-# RECOMMEND #
+#########################이 위=의 기능은 사용하지 않았음(장르생성, 장르수정)##################################################################
+## RECOMMEND ##
 
 @login_required
 def choice_create(request):
@@ -350,38 +351,3 @@ def recommend(request):
     return render(request, 'movies/recommand.html', context)
 
 
-# def recommand(request):
-#     movies = Movie.objects.order_by('-vote_average') #괜찮은 영화 순서대로 뽑기 위해서 -voteaverage로 정렬
-#     result = []
-#     if request.user.id != None: # 로그인한 유저라면
-#         like_movies = request.user.like_users # 로그인한 사람의 정보 저장
-#         if len(like_movies.all()) != 0: # 좋아요 한게 있어 그러면?
-#             choice_genres = [] # 비어있는 리스트 만들기
-#             for favorite_movie in like_movies.all(): # 이사람이 좋아한 영화를 하나씩 꺼내
-#                 for favorite_genre in favorite_movie.genre_ids.all(): # 좋아한 영화의 장르를 모두 꺼내
-#                     if favorite_genre not in choice_genres: # 중복되지 않게 넣기
-#                         choice_genres.append(favorite_genre)
-#             random.shuffle(choice_genres) # 리스트가 순서가 일정하면 같은 장르를 추천하기에 리스트를 한번 섞어준다.
-
-#             for movie in movies:
-#                 count = 1
-#                 for genre in choice_genres:
-#                     if genre in movie.genre_ids.all():
-#                         if movie not in result: # 중복되지 않게 넣기
-#                             result.append(movie)
-#                         # 영화 20개 될때 까지
-#                         if len(result) >= 20:
-#                             count = 0
-#                             break
-#                 if count == 0:
-#                         break
-#             # 매번 같은 영화를 추천하면 심심하기 때문에 20개의 영화를 리스트에 넣고 그 중에서 랜덤으로 10개를 뽑기
-#             results = random.sample(result, 10)
-#         else:  # 좋아요를 누른 영화(장르)가 없다면
-#             for k in range(15): # 평점 좋은 영화 15개 중에 10개 랜덤!
-#                 result.append(movies[k])
-#             results = random.sample(result,10)
-#     context = {
-#         'results': results,
-#     }
-#     return render(request, 'movies/recommand.html', context)
